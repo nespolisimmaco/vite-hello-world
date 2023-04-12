e087e258145b4d167b11ebdd3f370d70132798af
nespolisimmaco <snespoli@yahoo.com>
2023-04-12 17:22:01 +0200

    Add: created the component AppHero for jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script>
 import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue';
 
 export default {
   components: {
-    AppTitle
+    AppTitle,
+    AppHero
   },
   data() {
     return {
@@ -15,6 +17,7 @@ export default {
 
 <template>
   <AppTitle />
+  <AppHero />
 </template>
 
 <style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: "AppHero",
+    data() {
+        return {
+            title: "Tokyo"
+        }
+    }
+}
+</script>
+
+<template>
+    <h2>{{ title }}</h2>
+    <img class="jumbotron" src="../assets/img/Tokyo.jpg" alt="Tokyo">
+</template>
+
+<style>
+</style>
